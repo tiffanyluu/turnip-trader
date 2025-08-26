8268e00eaa4505a3eb6602d74824c8c35c21abd7
tiffanyluu <tiffanyluu555@gmail.com>
2025-08-25 20:42:38 -0500

    fix db pool

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -7,6 +7,9 @@ console.log("Connecting with:", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
